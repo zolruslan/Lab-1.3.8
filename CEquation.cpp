e168a769847ab7833bed4cf6b3a81c9245400709
Ruslan Kruhlov <zolruslan@ukr.net>
2016-12-01 16:06:53 +0200

    Created class and constructors

---
+++ CEquation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "CEquation.h"
+
+CEquation::CEquation () {
+}
+
+CEquation::CEquation (double x, double y, double z)
+    :x(x), y(y), z(z) {
+}
+
+CEquation::CEquation (CEquation &equation)
+    :x(equation.x), y(equation.y), z(equation.z) {
+}
--- CEquation.h
+++ CEquation.h
@@ -1,4 +1,13 @@
 #ifndef EXAM_H
 #define EXAM_H
 
+class CEquation {
+private:
+    double x, y, z;
+public:
+    CEquation ();
+    CEquation (double x, double y, double z);
+    CEquation (CEquation &equation);
+};
+
 #endif
