f3c50fb5ce27f44ff851244c08b1f54d7206970c
Ruslan Kruhlov <zolruslan@ukr.net>
2016-12-01 16:25:07 +0200

    Done!

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <Cmath>
+#include "CEquation.h"
+
+using namespace std;
+
+int main() {
+    CEquation *equation = new CEquation (-2.235*pow(10, -2), 2.23, 15.221);
+
+    cout << Equation (*equation) << endl;
+    return 0;
+}
