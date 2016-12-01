a73932dae0333f3aaa9f891b660f0d79b47f83f5
Ruslan Kruhlov <zolruslan@ukr.net>
2016-12-01 16:16:40 +0200

    Created formula

---
+++ Help.cpp
@@ -0,0 +1,7 @@
+#include <CMath>
+#include "CEquation.h"
+#include "Help.h"
+
+double Equation (CEquation &equation) {
+    return exp(fabs(equation.x-equation.y)) * pow(fabs(equation.x-equation.y), equation.x+equation.y) / (atan(equation.x) + atan(equation.z)) + pow(pow(equation.x, 6)+pow(log(equation.y), 2), 1/3);
+}
---
+++ Help.h
@@ -0,0 +1,8 @@
+#ifndef EXAM_H
+#define EXAM_H
+
+#include "CEquation.h"
+
+double Equation ();
+
+#endif
