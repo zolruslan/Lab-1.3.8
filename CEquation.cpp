04ecf3f6b7706dffa2578269d5f8f3266f372295
Ruslan Kruhlov <zolruslan@ukr.net>
2016-12-06 11:00:19 +0200

    Fix files Cequation.cpp and CEquation.h

--- CEquation.cpp
+++ CEquation.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <CMath>
 #include "CEquation.h"
 
 CEquation::CEquation () {
--- CEquation.h
+++ CEquation.h
@@ -4,6 +4,8 @@
 class CEquation {
 private:
     double x, y, z;
+    friend double Equation (CEquation &equation);
+
 public:
     CEquation ();
     CEquation (double x, double y, double z);
